efdbe22a87108904772a81e136613f186e63834a
lululombard <lululombard@gmail.com>
2020-04-10 00:55:40 +0000

    Add Redis

--- images/redcraft-mysql/tasks/install_mysql.sh
+++ images/redcraft-mysql/tasks/install_mysql.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-apt-get update
-sleep 1
 apt-get install -y mariadb-server
 
 systemctl enable mariadb
-systemctl start mariadb
 
-mysql < /tmp/create_mysql_accounts.sql
+mkdir -p /mnt/mysql_data
+
+echo "/dev/vdb /mnt/mysql_data ext4 rw" >> /etc/fstab
+
+sed -i 's/\/var\/lib\/mysql/\/mnt\/mysql_data\/mysql/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 echo '' > /root/.ssh/authorized_keys
---
+++ images/redcraft-redis/tasks/install_redis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+apt-get install -y redis-server
+
+systemctl enable redis-server
+
+sed -i 's/bind 127.0.0.1 ::1/#bind 127.0.0.1 ::1/g' /etc/redis/redis.conf
+sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf
+
+echo '' > /root/.ssh/authorized_keys
