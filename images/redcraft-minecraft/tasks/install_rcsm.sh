0850d2de7f960ca2d31e06b49308947561f9d842
lululombard <lululombard@gmail.com>
2020-10-27 00:06:29 -0400

    [WIP] Minecraft image

---
+++ images/redcraft-minecraft/tasks/install_rcsm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+# Add minecraft user
+adduser --system --no-create-home --group minecraft
+
+# Install rcsm
+URL=`curl -s https://api.github.com/repos/redcraft-org/redcraft_server_management/releases/latest | grep "amd64" | cut -d : -f 2,3 | tr -d \" | tail -n 1`
+wget -O /usr/bin/rcsm $URL
+chmod +x /usr/bin/rcsm
+
+# Config rcsm
+mv /tmp/rcsm_config /etc/rcsm_config
+
+sed -i "s/SETUP_AWS_ACCESS_KEY_ID\/$SETUP_AWS_ACCESS_KEY_ID/g" /etc/rcsm_config
+sed -i "s/SETUP_AWS_SECRET_ACCESS_KEY\/$SETUP_AWS_SECRET_ACCESS_KEY/g" /etc/rcsm_config
+sed -i "s/SETUP_WEBHOOKS_ENDPOINT\/$SETUP_WEBHOOKS_ENDPOINT/g" /etc/rcsm_config
+
+# Start rcsm at boot
+mv /tmp/rcsm.service /etc/systemd/system/rcsm.service
+systemctl daemon-reload
+systemctl enable rcsm
+
+# Setup disk
+mkdir -p /mnt/minecraft
+
+echo "/dev/sda /mnt/minecraft ext4 rw,_netdev" >> /etc/fstab
+
+cat << \EOF > /etc/rc.local
+#!/bin/bash
+
+# Make sure the storage partition is formatted
+
+blkid -o value -s TYPE /dev/sda 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+    mkfs -t ext4 /dev/sda
+    mount -a
+    chown -R minecraft:minecraft /mnt/minecraft
+    chmod +rw /mnt/minecraft
+fi
+EOF
+
+chmod +x /etc/rc.local
+
+echo '' > /root/.ssh/authorized_keys
