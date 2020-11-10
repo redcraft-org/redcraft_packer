b996e667742ecfbd30e563cde00423af5406d1fe
lululombard <lululombard@gmail.com>
2020-11-10 00:11:23 -0500

    [WIP] Django image

---
+++ images/redcraft-django/tasks/install_django.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Add gunicorn user
+adduser --system --no-create-home --group gunicorn
+mkdir /opt/redcraft_website
+chown -R gunicorn:gunicorn /opt/redcraft_website
+
+# Install redcraft_website
+python3 -m pip install gunicorn
+gunicorn git clone https://github.com/redcraft-org/redcraft_website.git
+
+# Config permissions and directories
+mkdir -p /var/log/redcraft
+chown -R gunicorn:gunicorn /opt/redcraft_website
+chown -R gunicorn:gunicorn /var/log/redcraft
+
+# Start gunicorn at boot
+mv /tmp/gunicorn.service /etc/systemd/system/gunicorn.service
+systemctl daemon-reload
+systemctl enable gunicorn
+
+echo '' > /root/.ssh/authorized_keys
